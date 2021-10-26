7a6cf1d2305d9ee435504cda5d8bb61166a70d1c
isabelasilva <isabelasilva@touchtec.com.br>
2021-10-26 19:29:23 -0300

    Testes de verificacao de login com sucesso e login com falha

--- src/test/java/LoginHeroku.java
+++ src/test/java/LoginHeroku.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -18,10 +19,26 @@ public class LoginHeroku {
     private By login = By.id("username");
     private By senha = By.id("password");
     private By botaoLogin = By.className("radius");
+    private By mensagem = By.id("flash");
+    private By sucesso = By.className("success");
+    private By erro = By.className("error");
 
     @Test
     public void login(){
         driver.get(url);
+        driver.findElement(login).sendKeys("tomsmith");
+        driver.findElement(senha).sendKeys("SuperSecretPassword!");
+        driver.findElement(botaoLogin).click();
+        Assertions.assertTrue(driver.findElement(sucesso) != null);
+    }
+
+    @Test
+    public void loginError(){
+        driver.get(url);
+        driver.findElement(login).sendKeys("tomsmith");
+        driver.findElement(senha).sendKeys("SenhaErrada");
+        driver.findElement(botaoLogin).click();
+        Assertions.assertTrue(driver.findElement(erro) != null);
     }
 
 }
